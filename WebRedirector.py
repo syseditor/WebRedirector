d8f9bbd850944521a948d4d02cdbc1c64c4bb077
immoorwhat <85906723+syseditor@users.noreply.github.com>
2021-10-24 17:05:56 +0300

    Add files via upload

---
+++ WebRedirector.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import webbrowser
+
+def func():
+    webbrowser.open_new_tab(e1.get())
+    root.destroy()
+
+def clear():
+    e1.delete(0, 100)
+
+root = Tk()
+root.title("Web Redirector")
+
+e1 = Entry(root)
+e1.grid(row=2, column=1, sticky=W)
+b1 = Button(root, text="Search", fg="green", bg="black", command=func).grid(row=3, column=1, sticky=W)
+b2 = Button(root, text="Clear", fg="red", bg="black", command=clear).grid(row=4, column=1, sticky=W)
+
+root.mainloop()
